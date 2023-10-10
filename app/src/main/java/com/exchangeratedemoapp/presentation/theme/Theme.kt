93ea90fb36e855334164402bd1c87db05e0c197d
Mariam Avanesyan <avanesyan.mariam98@gmail.com>
2023-10-10 12:03:57 +0400

    Add disable dark theme

--- app/src/main/java/com/exchangeratedemoapp/presentation/theme/Theme.kt
+++ app/src/main/java/com/exchangeratedemoapp/presentation/theme/Theme.kt
@@ -2,7 +2,6 @@ package com.exchangeratedemoapp.presentation.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -39,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun ExchangeRateDemoAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -58,7 +57,7 @@ fun ExchangeRateDemoAppTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Header.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
